2252a813ce33e76c3211b6e460ad419adb0bcd69
JimTBell <jimtbell@hotmail.com>
2022-08-10 14:29:02 +0100

    Test

---
+++ buildScript.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+echo "Creating File"
+touch BuildNewFile.txt
+ls
+
+echo "Deleting File"
+rm  BuildNewFile.txt
+
+echo "Make Folder"
+
+if [[ -d JIM ]]
+then
+    echo "JIM exists on your filesystem."
+	rm -rf JIM
+fi
+
+mkdir JIM
+touch BuildNewFile1.txt
+ls
+
+echo "Move file to folder"
+mv BuildNewFile1.txt JIM
+ls
+
+echo "Print out the current location"
+pwd
+
+echo "Prints out the current user"
+whoami
+
+
+echo "Adds a line of text to an existing file"
+echo "Start build" > build.log
+cat build.log
+echo
+echo "Started C# build" >> build.log
+cat build.log
+
+echo "prints out all of the files in a repo"
+git log --pretty=format: --name-status | cut -f2- | sort -u
+
+
+# - pushes a file to a different repo
